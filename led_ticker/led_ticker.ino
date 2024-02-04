3287bfae17cfc6607803b9cb359111519e387b5b
patrickr287 <priggall@hotmail.com>
2024-02-04 15:01:09 +0000

    led_ticker works, with button as input.

--- led_ticker/led_ticker.ino
+++ led_ticker/led_ticker.ino
@@ -2,24 +2,57 @@
 #include "Ticker.h"
 
 #define blueLED 7
+int btn = 3; //sets button
 
-bool ledState;
+bool blueLedState = 0;
+bool LED_state = 0;
+bool start_ticker = 0;
+bool stop_ticker = 1;
 
 void flipLED() {
-  ledState = !ledState;
+  blueLedState = !blueLedState;
+  digitalWrite(blueLED, blueLedState);
+  Serial.println("LED flipped!");
 }
 
-Ticker timer2(flipLED, 1000, 0, MILLIS);
+Ticker blueLEDticker(flipLED, 500, 0, MILLIS);
 
 void setup() {
-  //pinMode(LED_BUILTIN, OUTPUT); unsure what this does
+  pinMode(blueLED, OUTPUT);
+  pinMode(btn, INPUT);
   Serial.begin(9600);
   delay(2000);
-  timer2.start();
+  blueLEDticker.start();
+  Serial.println("Finished setup");
   }
 
-void loop() {
-  timer2.update();
-}
 
+void loop() {
+  if (digitalRead(btn) == 1) {
+    LED_state = !LED_state; //Indicates if LED sequence should be on or off, off = 0, on = 1.
+    if (LED_state == 0) {
+      stop_ticker = 1; //If want to turn off LED sequence, stop_ticker set to 1. Reset to 0 after running blueLEDticker.stop() once.
+    }
+    if (LED_state == 1) {
+      start_ticker = 1; ////If want to turn on LED sequence, start_ticker set to 1. Reset to 0 after running blueLEDticker.start() once.
+    }
+    Serial.println("Button pressed!");
+    delay(500);
+  }
+
+  if (start_ticker == 1) {
+    blueLEDticker.start();
+    Serial.println("LED seq should have started");
+    start_ticker = 0;
+  }
+
+  if (stop_ticker == 1) {
+    blueLEDticker.stop();
+    digitalWrite(blueLED, 0); //Make sure blueLEDticker doesnt end with blueLED still on
+    Serial.println("LED seq should have stopped");
+    stop_ticker = 0;
+  }
+
+  blueLEDticker.update(); //required to flip LED. Works different to the .detach() function we used in for coursework since those only work with ARM stuff.
+}
 
