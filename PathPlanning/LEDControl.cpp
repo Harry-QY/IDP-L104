cadb00dd588d1693de332dbe5b370d7332a7c055
patrickr287 <priggall@hotmail.com>
2024-02-05 14:51:55 +0000

    trying to add LEDs

---
+++ PathPlanning/LEDControl.cpp
@@ -0,0 +1,39 @@
+#include "LEDControl.h"
+#include "Ticker.h"
+
+#define blueLED 7 // blue LED
+
+Ticker blueLEDticker(flipLED, 500, 0, MILLIS);
+
+void flipblueLED(blueLED) {
+  bool blueLedState;
+  blueLedState = !blueLedState;
+  digitalWrite(blueLED, blueLedState);
+  Serial.println("LED flipped!");
+}
+
+void flipblueLEDsequence(bool LED_state, bool start_ticker, bool stop_ticker) {
+    LED_state = !LED_state; //Indicates if LED sequence should be on or off, off = 0, on = 1.
+      if (LED_state == 0) {
+        stop_ticker = 1; //If want to turn off LED sequence, stop_ticker set to 1. Reset to 0 after running blueLEDticker.stop() once.
+      }
+      if (LED_state == 1) {
+        start_ticker = 1; ////If want to turn on LED sequence, start_ticker set to 1. Reset to 0 after running blueLEDticker.start() once.
+      }
+    Serial.println("Button pressed!");
+    delay(500);
+
+    if (start_ticker == 1) {
+      blueLEDticker.start();
+      Serial.println("LED seq should have started");
+      start_ticker = 0;
+    }
+
+    if (stop_ticker == 1) {
+      blueLEDticker.stop();
+      digitalWrite(blueLED, 0); //Make sure blueLEDticker doesnt end with blueLED still on
+      Serial.println("LED seq should have stopped");
+      stop_ticker = 0;
+    }
+}
+
---
+++ PathPlanning/LEDControl.h
@@ -0,0 +1,9 @@
+#ifndef LEDCONTROL_H
+#define LEDCONTROL_H
+
+#include <arduino.h>
+
+void flipblueLED();
+void flipblueLEDsequence();
+
+#endif
